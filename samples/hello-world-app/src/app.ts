f5984a577f8afb7185af915daaec3021be736ede
Evelyn Eastmond <eveast@microsoft.com>
2020-03-24 10:24:37 -0400

    Moving up the hello world sample cube.

--- samples/hello-world-app/src/app.ts
+++ samples/hello-world-app/src/app.ts
@@ -77,7 +77,8 @@ export default class HelloWorld {
 				parentId: this.text.id,
 				transform: {
 					local: {
-						position: { x: 0, y: -1, z: 0 },
+						// position: { x: 0, y: -1, z: 0 },
+						position: { x: 0, y: 1, z: 0 },
 						scale: { x: 0.4, y: 0.4, z: 0.4 }
 					}
 				}
